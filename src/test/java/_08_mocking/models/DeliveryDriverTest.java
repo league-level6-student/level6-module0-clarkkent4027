470ee6f7d80c8b776705aff675e6028e80250181
Student <league@iMac-16.local>
2024-03-31 11:28:47 -0700

    WhiteBox

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -12,37 +12,58 @@ import static org.mockito.Mockito.when;
 class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
+    String name;
+
+    @Mock
+    Car car;
+
+    @Mock
+    CellPhone cellPhone;
 
     @BeforeEach
     void setUp() {
-
+    MockitoAnnotations.openMocks(this);
+    DeliveryDriver d = new DeliveryDriver(name, car, cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+        boolean expected = true;
 
         //when
-
+        when(cellPhone.browseCatMemes()).thenReturn(true);
+        boolean actual = cellPhone.browseCatMemes();
         //then
+        assertEquals(expected, actual);
     }
 
     @Test
     void itShouldRefuel() {
         //given
+        boolean expectedRefuel = true;
+        int octane = 50;
 
         //when
+        when(car.fillTank(octane)).thenReturn(true);
+        boolean actualRefuel = car.fillTank(octane);
 
         //then
+        assertEquals(expectedRefuel, actualRefuel);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
+        boolean expected = true;
+        String num = "911-911-911";
 
         //when
+        when(cellPhone.call(num)).thenReturn(true);
+        boolean actual = cellPhone.call(num);
 
         //then
+        assertEquals(expected, actual);
     }
 
 }
--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -17,27 +17,57 @@ class MyDonutShopTest {
 
     MyDonutShop myDonutShop;
 
+    @Mock
+    DeliveryService deliveryService;
+
+    @Mock
+    BakeryService bakeryService;
+
+    @Mock
+    PaymentService paymentService;
+
+
     @BeforeEach
     void setUp() {
-
+    MockitoAnnotations.openMocks(this);
+     myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
         //given
+        Order o = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
 
         //when
+        myDonutShop.openForTheDay();
+        myDonutShop.takeOrder(o);
 
         //then
+        verify(myDonutShop, times(1)).addOrder(o);
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
+        Order o = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
 
         //when
+        myDonutShop.openForTheDay();
 
         //then
+        Throwable exceptionThrown = assertThrows(Exception.class, () -> myDonutShop.takeOrder(o));
+        assertEquals(exceptionThrown.getMessage(), "Insufficient donuts remaining");
+        verify(myDonutShop, never()).addOrder(o);
     }
 
     @Test
